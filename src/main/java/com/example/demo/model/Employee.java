3cee138affce413189381087f2d7bd00f7111efa
bala-amypo <bala@amypo.in>
2025-12-18 15:33:37 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.presistance.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Employee{
+    @Id
+    @GeneratedValue(stratergy = GenerationType.IDENTITY)
+    private Long id;
     
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private S
 }
