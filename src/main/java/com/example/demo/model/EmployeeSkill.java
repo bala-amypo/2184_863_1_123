20d091c8a47435b775ba4dbaa1fefcc825be1bd6
bala-amypo <bala@amypo.in>
2025-12-18 16:33:38 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeSkill.java
+++ src/main/java/com/example/demo/model/EmployeeSkill.java
@@ -1,5 +1,10 @@
 package com.example.demo.model;
 
 public class EmployeeSkill{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String proficiencyLevel;
+    
 }
