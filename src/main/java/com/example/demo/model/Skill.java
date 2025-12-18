2ae40c2920e71f04e0fcaea8683e9bfa4d4dac39
bala-amypo <bala@amypo.in>
2025-12-18 15:54:38 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -1,5 +1,16 @@
 package com.example.demo.model;
 
+import jarkarta.persistence.*;
+
+@Entity
 public class Skill{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String 
 }
