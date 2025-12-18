962c416c04115cb50da2f87d20fa2164e1cd382c
bala-amypo <bala@amypo.in>
2025-12-18 16:36:38 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeSkill.java
+++ src/main/java/com/example/demo/model/EmployeeSkill.java
@@ -6,5 +6,22 @@ public class EmployeeSkill{
     private Long id;
 
     private String proficiencyLevel;
-    
+    private Integer yearOfExperience;
+    private Boolean active = true;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getProficiencyLevel(){
+        return proficiencyLevel;
+    }
+    public void ProficiencyLevel(String category){
+        this.category = category;
+    }
+
+
+
 }
