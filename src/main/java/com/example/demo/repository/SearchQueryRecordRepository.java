19211c5f99baebe9b958572365af856fa3312915
bala-amypo <bala@amypo.in>
2025-12-20 06:43:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/SearchQueryRecordRepository.java
+++ src/main/java/com/example/demo/repository/SearchQueryRecordRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public class SearchQueryRecordRepository{
-    
+import com.example.demo.model.SearchQueryRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface SearchQueryRecordRepository extends JpaRepository<SearchQueryRecord, Long> {
+
+    List<SearchQueryRecord> findBySearcherId(Long userId);
 }
