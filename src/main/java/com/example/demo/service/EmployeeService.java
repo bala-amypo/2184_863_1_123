7402aebdb267e867c0d7e6e82e095d9d84492456
bala-amypo <bala@amypo.in>
2025-12-20 06:37:27 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
-public class EmployeeService{
-    
+import com.example.demo.model.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+    Employee updateEmployee(Long id, Employee employee);
+    Employee getEmployeeById(Long id);
+    List<Employee> getAllEmployees(boolean onlyActive);
+    void deactivateEmployee(Long id);
 }
