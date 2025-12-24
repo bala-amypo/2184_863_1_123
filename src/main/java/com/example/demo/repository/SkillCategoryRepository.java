63ed3dd291f1a98af2869d9318437bce8420d470
bala-amypo <bala@amypo.in>
2025-12-24 04:17:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillCategoryRepository.java
+++ src/main/java/com/example/demo/repository/SkillCategoryRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.SkillCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.model.SkillCategory;
 import java.util.Optional;
 
 public interface SkillCategoryRepository extends JpaRepository<SkillCategory, Long> {
-
+    boolean existsByCategoryName(String categoryName);
     Optional<SkillCategory> findByCategoryName(String categoryName);
 }
